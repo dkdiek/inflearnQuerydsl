c3b129f3463494c5013bc2d730119f63e6772387
Cliff <developerlyk@gmail.com>
2024-05-18 21:48:56 +0900

    Querydsl where sort

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -5,6 +5,7 @@ import static study.querydsl.Entity.QMember.*;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,11 @@ import study.querydsl.Entity.Team;
 public class QuerydslBasicTest {
   @Autowired EntityManager em;
   JPAQueryFactory queryFactory;
-  
-  
+
   @BeforeEach
   public void before() {
     JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-    
+
     Team teamA = new Team("teamA");
     Team teamB = new Team("teamB");
     em.persist(teamA);
@@ -49,8 +49,43 @@ public class QuerydslBasicTest {
 
   @Test
   public void startQuerydsl() {
-    Member findMember = queryFactory.selectFrom(member).where(member.username.eq("member1")).fetchOne();
+    Member findMember =
+        queryFactory.selectFrom(member).where(member.username.eq("member1")).fetchOne();
 
     assertThat(findMember.getUsername()).isEqualTo("member1");
   }
+
+  @Test
+  public void serach() {
+    Member findMember =
+        queryFactory
+            .selectFrom(member)
+            .where(member.username.eq("member1"), (member.age.eq(10)))
+            .fetchOne();
+
+    assertThat(findMember.getUsername()).isEqualTo("member1");
+  }
+
+  @Test
+  public void resultFetch() {
+    List<Member> fetch = queryFactory.selectFrom(member).fetch();
+    Member fetchOne = queryFactory.selectFrom(member).fetchOne();
+    Member fetchFirst = queryFactory.selectFrom(member).fetchFirst();
+    Long totalCount = queryFactory.select(member.count()).from(member).fetchOne();
+  }
+  
+  @Test
+  public void sort(){
+    em.persist(new Member(null,100));
+    em.persist(new Member("member5",100));
+    em.persist(new Member("member6",100));
+
+    queryFactory
+        .selectFrom(member)
+        .where(member.age.eq(100))
+        .orderBy(member.age.desc(), member.username.asc().nullsLast())
+        .fetch();
+
+  }
+
 }
