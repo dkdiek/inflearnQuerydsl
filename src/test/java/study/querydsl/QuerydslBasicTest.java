64d0009f0575ccdf855294a16a464de681d92098
Cliff <developerlyk@gmail.com>
2024-05-19 15:06:07 +0900

    theta join

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,7 +2,9 @@ package study.querydsl;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.Entity.QMember.*;
+import static study.querydsl.Entity.QTeam.*;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
@@ -22,7 +24,7 @@ public class QuerydslBasicTest {
 
   @BeforeEach
   public void before() {
-    JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+    queryFactory = new JPAQueryFactory(em);
 
     Team teamA = new Team("teamA");
     Team teamB = new Team("teamB");
@@ -73,19 +75,89 @@ public class QuerydslBasicTest {
     Member fetchFirst = queryFactory.selectFrom(member).fetchFirst();
     Long totalCount = queryFactory.select(member.count()).from(member).fetchOne();
   }
-  
+
   @Test
-  public void sort(){
-    em.persist(new Member(null,100));
-    em.persist(new Member("member5",100));
-    em.persist(new Member("member6",100));
+  public void sort() {
+    em.persist(new Member(null, 100));
+    em.persist(new Member("member5", 100));
+    em.persist(new Member("member6", 100));
 
     queryFactory
         .selectFrom(member)
         .where(member.age.eq(100))
         .orderBy(member.age.desc(), member.username.asc().nullsLast())
         .fetch();
-
   }
 
+  @Test
+  public void paging1() {
+    List<Member> result =
+        queryFactory.selectFrom(member).orderBy(member.username.desc()).offset(1).limit(2).fetch();
+  }
+
+  @Test
+  public void paging2() {
+    Long totalCount =
+        queryFactory.select(member.count()).from(member).offset(1).limit(2).fetchOne();
+  }
+
+  @Test
+  public void aggregation() {
+    List<Tuple> result =
+        queryFactory
+            .select(
+                member.count(),
+                member.age.sum(),
+                member.age.avg(),
+                member.age.max(),
+                member.age.min())
+            .from(member)
+            .fetch();
+
+    Tuple tuple = result.get(0);
+    assertThat(tuple.get(member.count())).isEqualTo(4);
+    assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+    assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+    assertThat(tuple.get(member.age.max())).isEqualTo(40);
+    assertThat(tuple.get(member.age.min())).isEqualTo(10);
+  }
+
+  @Test
+  void group() throws Exception {
+    List<Tuple> result =
+        queryFactory
+            .select(team.name, member.age.avg())
+            .from(member)
+            .join(member.team, team)
+            .groupBy(team.name)
+            .fetch();
+    Tuple teamA = result.get(0);
+    Tuple teamB = result.get(1);
+
+    assertThat(teamA.get(team.name)).isEqualTo("teamA");
+    assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+    assertThat(teamB.get(team.name)).isEqualTo("teamB");
+    assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+  }
+
+  @Test
+  void join() throws Exception {
+    List<Member> result =
+        queryFactory
+            .selectFrom(member)
+            .join(member.team, team)
+            .where(team.name.eq("teamA"))
+            .fetch();
+
+    assertThat(result).extracting("username").containsExactly("member1", "member2");
+  }
+
+  @Test
+  void theta_join() throws Exception {
+    em.persist(new Member("teamA"));
+    em.persist(new Member("teamB"));
+
+    queryFactory.select(member).from(member, team).on(member.username.eq(team.name)).fetch();
+  }
 }
