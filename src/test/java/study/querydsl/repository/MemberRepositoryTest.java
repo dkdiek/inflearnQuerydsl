aa42cc6ecc9d072efd4deddbd1a9eb6968e11c3d
Cliff <developerlyk@gmail.com>
2024-05-21 21:43:24 +0900

    QuerydslPredicateExecutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package study.querydsl.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.Entity.QMember.member;
 
 import jakarta.persistence.EntityManager;
 import java.util.List;
@@ -89,5 +90,25 @@ public class MemberRepositoryTest {
         .containsExactly("member1", "member2", "member3");
   }
 
+  @Test
+  void querydslPredicateExecutorTest() throws Exception {
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    em.persist(teamA);
+    em.persist(teamB);
+    Member member1 = new Member("member1", 10, teamA);
+    Member member2 = new Member("member2", 20, teamA);
+    Member member3 = new Member("member3", 30, teamB);
+    Member member4 = new Member("member4", 40, teamB);
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
+    
+    Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+    for (Member memberIter : result) {
+      System.out.println("member1" + memberIter);
+    }
+  }
 }
 
