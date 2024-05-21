d9afbcc98fce6b2841ed78846d0b7d4f80ebde14
Cliff <developerlyk@gmail.com>
2024-05-21 22:21:20 +0900

    Querydsl 지원 클래스

--- src/main/java/study/querydsl/repository/MemberTestRepository.java
+++ src/main/java/study/querydsl/repository/MemberTestRepository.java
@@ -26,65 +26,77 @@ public class MemberTestRepository extends Querydsl4RepositorySupport {
   public MemberTestRepository() {
     super(Member.class);
   }
+
   public List<Member> basicSelect() {
-    return select(member)
-            .from(member)
-            .fetch();
+    return select(member).from(member).fetch();
   }
+
   public List<Member> basicSelectFrom() {
-    return selectFrom(member)
-            .fetch();
+    return selectFrom(member).fetch();
   }
-  public Page<Member> searchPageByApplyPage(MemberSearchCondition condition,
-                                            Pageable pageable) {
-    JPAQuery<Member> query = selectFrom(member)
+
+  public Page<Member> searchPageByApplyPage(MemberSearchCondition condition, Pageable pageable) {
+    JPAQuery<Member> query =
+        selectFrom(member)
             .leftJoin(member.team, team)
-            .where(usernameEq(condition.getUsername()),
-                    teamNameEq(condition.getTeamName()),
-                    ageGoe(condition.getAgeGoe()),
-                    ageLoe(condition.getAgeLoe()));
-    List<Member> content = getQuerydsl().applyPagination(pageable, query)
-            .fetch();
-    return PageableExecutionUtils.getPage(content, pageable,
-            query::fetchCount);
+            .where(
+                usernameEq(condition.getUsername()),
+                teamNameEq(condition.getTeamName()),
+                ageGoe(condition.getAgeGoe()),
+                ageLoe(condition.getAgeLoe()));
+    List<Member> content = getQuerydsl().applyPagination(pageable, query).fetch();
+    return PageableExecutionUtils.getPage(content, pageable, query::fetchCount);
   }
-  public Page<Member> applyPagination(MemberSearchCondition condition,
-                                      Pageable pageable) {
-    return applyPagination(pageable, contentQuery -> contentQuery
-            .selectFrom(member)
-            .leftJoin(member.team, team)
-            .where(usernameEq(condition.getUsername()),
+
+  public Page<Member> applyPagination(MemberSearchCondition condition, Pageable pageable) {
+    return applyPagination(
+        pageable,
+        contentQuery ->
+            contentQuery
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                    usernameEq(condition.getUsername()),
                     teamNameEq(condition.getTeamName()),
                     ageGoe(condition.getAgeGoe()),
                     ageLoe(condition.getAgeLoe())));
   }
-  public Page<Member> applyPagination2(MemberSearchCondition condition,
-                                       Pageable pageable) {
-    return applyPagination(pageable, contentQuery -> contentQuery
-                    .selectFrom(member)
-                    .leftJoin(member.team, team)
-                    .where(usernameEq(condition.getUsername()),
-                            teamNameEq(condition.getTeamName()),
-                            ageGoe(condition.getAgeGoe()),
-                            ageLoe(condition.getAgeLoe())),
-            countQuery -> countQuery
-                    .selectFrom(member)
-                    .leftJoin(member.team, team)
-                    .where(usernameEq(condition.getUsername()),
-                            teamNameEq(condition.getTeamName()),
-                            ageGoe(condition.getAgeGoe()),
-                            ageLoe(condition.getAgeLoe()))
-    );
+
+  public Page<Member> applyPagination2(MemberSearchCondition condition, Pageable pageable) {
+    return applyPagination(
+        pageable,
+        contentQuery ->
+            contentQuery
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                    usernameEq(condition.getUsername()),
+                    teamNameEq(condition.getTeamName()),
+                    ageGoe(condition.getAgeGoe()),
+                    ageLoe(condition.getAgeLoe())),
+        countQuery ->
+            countQuery
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                    usernameEq(condition.getUsername()),
+                    teamNameEq(condition.getTeamName()),
+                    ageGoe(condition.getAgeGoe()),
+                    ageLoe(condition.getAgeLoe())));
   }
+
   private BooleanExpression usernameEq(String username) {
     return isEmpty(username) ? null : member.username.eq(username);
   }
+
   private BooleanExpression teamNameEq(String teamName) {
     return isEmpty(teamName) ? null : team.name.eq(teamName);
   }
+
   private BooleanExpression ageGoe(Integer ageGoe) {
     return ageGoe == null ? null : member.age.goe(ageGoe);
   }
+
   private BooleanExpression ageLoe(Integer ageLoe) {
     return ageLoe == null ? null : member.age.loe(ageLoe);
   }
